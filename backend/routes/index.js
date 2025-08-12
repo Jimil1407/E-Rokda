020401445d674990e9d8f7eec2acb866e5798e5b
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-08-12 19:17:00 +0530

    added express routing and cors

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,47 @@
 import express from "express";
+import zod from "zod";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
 const rootRouter = express.Router();
 
-rootRouter.get("/", (req, res) => {
-    res.json({ message: "Paytm Project API is running!" });
+const JWT_SECRET = process.env.JWT_SECRET;
+
+rootRouter.post("/signup", async (req, res) => {
+    try {
+        const userSchema = zod.object({
+            firstName: zod.string().min(3).max(50),
+            lastName: zod.string().min(3).max(50),
+            password: zod.string().min(8).max(50),
+            email: zod.string().email(),
+        });
+        const { firstName, lastName, password, email } = userSchema.parse(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ firstName, lastName, password: hashedPassword, email });
+        res.status(200).json({ message: "User created successfully", user } );
+    } catch (error) {
+        res.status(500).json({ message: "Error creating user", error });
+    }
 });
+
+    rootRouter.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
+        res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        res.status(500).json({ message: "Error logging in", error });
+    }
+});
+
 
 export default rootRouter;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,48 +1,9 @@
 import express from "express";
-import zod from "zod";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 import { verifyToken } from "../middleware/verify.js";
 
 const userRouter = express.Router();
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
-userRouter.post("/signup", async (req, res) => {
-    try {
-        const userSchema = zod.object({
-            firstName: zod.string().min(3).max(50),
-            lastName: zod.string().min(3).max(50),
-            password: zod.string().min(8).max(50),
-            email: zod.string().email(),
-        });
-        const { firstName, lastName, password, email } = userSchema.parse(req.body);
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ firstName, lastName, password: hashedPassword, email });
-        res.status(200).json({ message: "User created successfully", user } );
-    } catch (error) {
-        res.status(500).json({ message: "Error creating user", error });
-    }
-});
-
-userRouter.post("/signin", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ message: "Invalid email or password" });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Invalid email or password" });
-        }
-        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
-        res.status(200).json({ message: "Login successful", token });
-    } catch (error) {
-        res.status(500).json({ message: "Error logging in", error });
-    }
-});
 
 userRouter.put("/update", verifyToken, async (req, res) => {
     try {
