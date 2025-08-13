7b98665b13fa1301e8672c1dec70eb2896d2f417
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-08-13 19:26:03 +0530

    render deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -21,7 +21,12 @@ rootRouter.post("/signup", async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({ firstName, lastName, password: hashedPassword, email });
         const balance = await Balance.create({ userId: user._id, balance: 1000 });
-        res.status(200).json({ message: "User created successfully", user, balance } );
+        res.status(200).json({ message: "User created successfully", user: {
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        }, balance } );
     } catch (error) {
         res.status(500).json({ message: "Error creating user", error });
     }
@@ -39,7 +44,12 @@ rootRouter.post("/signup", async (req, res) => {
             return res.status(401).json({ message: "Invalid email or password" });
         }
         const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
-        res.status(200).json({ message: "Login successful", token, user });
+        res.status(200).json({ message: "Login successful", token, user: {
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        } });
     } catch (error) {
         res.status(500).json({ message: "Error logging in", error });
     }
